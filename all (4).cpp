eb339655c6830bc457fe4ab8dedf3d2634e9df12
alexaqo <gdesemechki@gmail.com>
2025-11-11 09:00:06 +0300

    Add files via upload

--- all (4).cpp
+++ all (4).cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main() 
 {
-	cout << "x \t y \t && \t ||" << endl;
-	cout << "0 \t 0 \t " << (0 && 0) << " \t " << (0 || 0) << endl;
-	cout << "0 \t 1 \t " << (0 && 1) << " \t " << (0 || 1) << endl;
-	cout << "1 \t 0 \t " << (1 && 0) << " \t " << (1 || 0) << endl;
-	cout << "1 \t 1 \t " << (1 && 1) << " \t " << (1 || 1) << endl;
+	int x = 3;
+	cout << sizeof (int) << endl;
+	cout << sizeof (x * 10) << endl;
+	cout << sizeof (x * 0.1) << endl;
 	return 0;
 
 }
